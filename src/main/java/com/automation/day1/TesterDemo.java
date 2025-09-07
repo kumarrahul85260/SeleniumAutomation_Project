99ca78408f2299132cba6e0cb2b2de895366bafd
Rahul Kumar <kumarrahul85260@gmail.com>
2025-09-07 18:01:21 +0530

    testcase 1

---
+++ src/main/java/com/automation/day1/TesterDemo.java
@@ -0,0 +1,14 @@
+package com.automation.day1;
+
+import org.testng.annotations.Test;
+
+public class TesterDemo {
+		@Test
+		public void SIgnIn() {
+			System.out.println("t1");
+		}
+		@Test
+		public void SignIn() {
+			System.out.println("t2");
+		}
+}
