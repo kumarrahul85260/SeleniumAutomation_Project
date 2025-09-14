300e5f77faf5680ec65e526f8ec023e32316e6dc
Rahul Kumar <kumarrahul85260@gmail.com>
2025-09-14 13:17:43 +0530

    add testcase

---
+++ src/main/java/com/test/day3/LoginTest.java
@@ -0,0 +1,25 @@
+package com.test.day3;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	
+	@BeforeMethod
+	public void setUp() {
+		System.out.println("launching the browsers.....");
+	}
+	@Test
+	public void getTitle() {
+		Assert.assertEquals(true,false);
+				
+		}
+	
+	@AfterMethod
+	public void tearDown() {
+		System.out.println("closing the browwser");
+	}
+}
